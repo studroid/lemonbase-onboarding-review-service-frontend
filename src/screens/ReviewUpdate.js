dea434af5e00ccdbf08ae96f330d58e3970a4648
SJKim <ksjksj0122@gmail.com>
2020-09-17 23:34:27 +0900

    Implement deleting a policy

--- src/screens/ReviewUpdate.js
+++ src/screens/ReviewUpdate.js
@@ -1,7 +1,7 @@
-import React, {useEffect, useState} from 'react';
+import React, {useState} from 'react';
 import {useHistory, useParams} from 'react-router-dom';
 import ReviewDetail from '../components/ReviewDetail';
-import {Error} from '../components/StyledComponents';
+import {Error, SmallButton} from '../components/StyledComponents';
 import APIHandler from '../APIHandler';
 import ReviewList from './ReviewList';
 
@@ -11,7 +11,7 @@ function ReviewUpdate(props) {
   const history = useHistory();
   const {id} = useParams();
 
-  function onSubmitClicked(detail_data) {
+  function onUpdateClicked(detail_data) {
     APIHandler.put(`/policy/${id}/`, detail_data).then(result => {
       if (result.status == 200) {
         alert('리뷰 정책이 변경되었습니다!');
@@ -25,12 +25,26 @@ function ReviewUpdate(props) {
     });
   }
 
+  function onDeleteClick() {
+    APIHandler.delete(`/policy/${id}`).then(result => {
+      if (result.status == 204) {
+        alert('리뷰 정책이 삭제되었습니다.');
+        history.push(ReviewList.routeName);
+      } else {
+        setIsError(true);
+      }
+    }).catch(e => {
+      setIsError(true);
+    });
+  }
+
   return (
       <div>
         <h2>ReviewUpdate{id}</h2>
-        <ReviewDetail buttonName='수정하기' onSubmitClicked={onSubmitClicked}
+        <ReviewDetail buttonName='수정하기' onSubmitClicked={onUpdateClicked}
                       defaultData={props.location.state.defaultData}/>
-        {isError && <Error>정책 수정 도중 문제가 발생했습니다. 입력을 다시 확인하세요!</Error>}
+        <SmallButton onClick={onDeleteClick}>삭제하기</SmallButton>
+        {isError && <Error>처리 도중 문제가 발생했습니다. 입력을 다시 확인하세요!</Error>}
       </div>
   );
 }
